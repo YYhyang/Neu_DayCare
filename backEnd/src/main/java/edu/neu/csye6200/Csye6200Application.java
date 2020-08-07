b8fc68838f108750afbc8f690ec60b8f8c4dca28
YYhyang <44155580+YYhyang@users.noreply.github.com>
2020-08-07 12:26:09 +0800

    Create backend

---
+++ backEnd/src/main/java/edu/neu/csye6200/Csye6200Application.java
@@ -0,0 +1,13 @@
+package edu.neu.csye6200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csye6200Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csye6200Application.class, args);
+    }
+
+}
