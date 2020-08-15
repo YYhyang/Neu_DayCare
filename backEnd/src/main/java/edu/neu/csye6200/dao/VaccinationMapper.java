eb7855e892926984c3031362a74f02ee6d64a92b
YYhyang <44155580+YYhyang@users.noreply.github.com>
2020-08-16 02:22:08 +0800

    three methods in vaccination controller

--- backEnd/src/main/java/edu/neu/csye6200/dao/VaccinationMapper.java
+++ backEnd/src/main/java/edu/neu/csye6200/dao/VaccinationMapper.java
@@ -2,6 +2,9 @@ package edu.neu.csye6200.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import edu.neu.csye6200.entity.dto.VaccinationDO;
+import edu.neu.csye6200.entity.vo.VaccinationVO;
+
+import java.util.List;
 
 public interface VaccinationMapper extends BaseMapper<VaccinationDO> {
 
--- backEnd/src/main/java/edu/neu/csye6200/web/VaccinationController.java
+++ backEnd/src/main/java/edu/neu/csye6200/web/VaccinationController.java
@@ -1,13 +1,27 @@
 package edu.neu.csye6200.web;
 
+import com.baomidou.mybatisplus.extension.api.R;
 import edu.neu.csye6200.base.BaseController;
+import edu.neu.csye6200.base.Result;
+import edu.neu.csye6200.base.annotation.LogOperate;
+import edu.neu.csye6200.entity.dto.VaccinationDO;
+import edu.neu.csye6200.entity.vo.VaccinationVO;
 import edu.neu.csye6200.service.VaccinationService;
+import edu.neu.csye6200.utils.ConverterUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Vector;
 
 /**
- * @author Caspar
+ * @author Caspar Yuhan Yang
  * @date 2020/8/14 13:36
  */
+@RestController
+@RequestMapping("/vaccination")
+@Slf4j
 public class VaccinationController extends BaseController {
 
     @Autowired
@@ -30,6 +44,36 @@ public class VaccinationController extends BaseController {
      * PS. 在Controller层只调用Service层接口，业务逻辑在Service层调用
      */
 
+    @GetMapping(value = "/listByStudentId/{studentId}")
+    @LogOperate(value = "find by student id")
+    public Result<Object> listByStudentId(@PathVariable Integer studentId){
+        List<VaccinationVO> vaccinationVOS= new Vector<>();
+        ConverterUtils.convertList(vaccinationService.getListVaccination(studentId),vaccinationVOS,VaccinationVO.class);
+        return Result.buildOkData(vaccinationVOS);
+    }
+
+    @GetMapping(value = "/getByStudentIdAndImmunizationName/{studentId}/{immunizationName}")
+    @LogOperate(value = "find by student id and immunization name")
+    public Result<Object> getByStudentIdAndImmunizationName(@PathVariable Integer studentId,@PathVariable String immunizationName){
+        VaccinationVO vaccinationVO=new VaccinationVO();
+        ConverterUtils.convert(vaccinationService.getVaccination(studentId,immunizationName),vaccinationVO);
+        return Result.buildOkData(vaccinationVO);
+    }
+
+    @PostMapping(value = "/add")
+    @LogOperate(value = "add one record")
+    public Result<Object> add(@RequestBody VaccinationVO vaccinationVO){
+        VaccinationDO vaccinationDO=new VaccinationDO();
+        ConverterUtils.convert(vaccinationVO,vaccinationDO);
+        boolean insert=vaccinationService.save(vaccinationDO);
+        if(insert)
+        {
+            return Result.buildOkData(vaccinationDO);
+        }
+        else {
+            return Result.buildFailData(vaccinationDO);
+        }
+    }
 
 
 }
